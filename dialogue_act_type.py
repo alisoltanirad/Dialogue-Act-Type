b56c71bd6c164603ad5daf6a25eabddf7018f37b
alisoltanirad <soltaniradali@gmail.com>
2020-11-12 19:56:07 +0330

    add get_posts function

--- dialogue_act_type.py
+++ dialogue_act_type.py
@@ -1,9 +1,21 @@
-#
+# https://github.com/alisoltanirad/Dialogue-Act-Type
 # Dependencies: nltk
+import ssl
 import nltk
 
 def main():
+    classify_act_type(get_posts())
+
+
+def classify_act_type(posts):
     pass
+
+
+def get_posts():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    nltk.download('nps_chat')
+    posts = nltk.corpus.nps_chat.xml_posts()
+    return posts
 
 
 if __name__ == '__main__':
