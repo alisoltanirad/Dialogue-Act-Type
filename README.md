3b4aab34c5aa40d54c61679b5a4f00937848fea1
alisoltanirad <soltaniradali@gmail.com>
2020-11-12 15:57:19 +0330

    initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dialogue Act Type Classifier
+
+Python code to classify dialogue act type.
---
+++ dialogue_act_type.py
@@ -0,0 +1,10 @@
+#
+# Dependencies: nltk
+import nltk
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
